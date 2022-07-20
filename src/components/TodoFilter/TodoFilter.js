e398ff816c39215404c10ebddda0d0ffde0b05e1
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-07-20 21:00:05 +0530

    initial commit

--- src/components/TodoFilter/TodoFilter.js
+++ src/components/TodoFilter/TodoFilter.js
@@ -1,4 +1,11 @@
+// - Your challenge is to add a filter to do the following
+//     - Show completed todos
+//     - Show incomplete todos
+//     - Show all todos
+
 import { useState } from "react";
+
+import "./TodoFilter.css";
 
 const TodoFilter = () => {
   const todos = [
@@ -24,7 +31,63 @@ const TodoFilter = () => {
     }
   ];
 
-  const [todoList, setTodoList] = useState(todos);
+  const filterChoices = ["Show Completed", "Show InCompleted", "Show All"];
+
+  const [todoList, setTodoList] = useState({
+    selectedFilter: "Show All",
+    list: todos
+  });
+
+  const { selectedFilter, list } = todoList;
+
+  const optionSelected = (option) => {
+    switch (option) {
+      case "Show Completed":
+        setTodoList({
+          selectedFilter: option,
+          list: todos.filter((item) => item.completed)
+        });
+        break;
+      case "Show InCompleted":
+        setTodoList({
+          selectedFilter: option,
+          list: todos.filter((item) => item.completed === false)
+        });
+        break;
+      case "Show All":
+        setTodoList({
+          selectedFilter: option,
+          list: todos
+        });
+        break;
+      default:
+        setTodoList(todoList);
+    }
+  };
+
+  return (
+    <div className="todoFilter">
+      <div className="filter__choices">
+        {filterChoices.map((option) => (
+          <button
+            key={option}
+            className={selectedFilter === option ? "selected" : "choice"}
+            onClick={() => optionSelected(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+      <div className="TodoList">
+        {list.map((item) => (
+          <div className="todo__item" key={item.id}>
+            <h3>{item.title}</h3>
+            <h5>{item.completed ? "Completed" : "Incomplete"}</h5>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export { TodoFilter };
